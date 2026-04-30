f20ca10d9ee56d6f5a366636dd75da8dec5435cf
tamse94 <kopetraja254@gmail.com>
2026-05-01 03:36:08 +0700

    Update route.js

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,14 +1,26 @@
-import NextAuth from "next-auth"
-import FacebookProvider from "next-auth/providers/facebook"
+import NextAuth from "next-auth";
+import FacebookProvider from "next-auth/providers/facebook";
 
-const handler = NextAuth({
+export const authOptions = {
   providers: [
     FacebookProvider({
       clientId: process.env.FACEBOOK_CLIENT_ID,
       clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
     }),
   ],
-  secret: process.env.NEXTAUTH_SECRET,
-})
+  callbacks: {
+    async jwt({ token, account }) {
+      if (account) {
+        token.accessToken = account.access_token;
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      session.accessToken = token.accessToken;
+      return session;
+    },
+  },
+};
 
-export { handler as GET, handler as POST }
+const handler = NextAuth(authOptions);
+export { handler as GET, handler as POST };
